f44dbb32ac1d35775bffadf906dcd12e225e8fef
Mitch Goshorn <msgoshorn@gmail.com>
2020-04-28 12:01:05 -0400

    Initial commit

---
+++ data-types.js
@@ -0,0 +1,44 @@
+let x = 10;
+let str = 'Hello world';
+let bool = true;
+let arr = [1, 2, 3];
+let date = Date();
+let symbol = Symbol('myApp');
+let myUndefined = undefined;
+let myNull = null;
+let bigInt = BigInt(100);
+let myObject = {
+    myKey: 'myValue',
+    a: 'Hello'
+};
+
+// Arrow Function (lambda function)
+// let myFunction = () => {};
+
+function myFunction() {
+
+}
+
+/*
+    JavaScript Types
+    1. number - (Java Double)
+    2. string
+    3. boolean
+    4. object
+    5. symbol
+    6. undefined
+    7. bigint
+    8. function (not necessarily a data type)
+*/
+
+console.log(typeof x); // number
+console.log(typeof str); // string
+console.log(typeof bool); // boolean
+console.log(typeof arr); // object
+console.log(typeof date); // string
+console.log(typeof symbol); // symbol
+console.log(typeof myUndefined); // undefined
+console.log(typeof myNull); // object
+console.log(typeof bigInt); // bigint
+console.log(typeof myObject); // object
+console.log(typeof myFunction); // function
---
+++ scopes.js
@@ -0,0 +1,65 @@
+/*
+    Variable declaration and Variable Scope
+    JavaScript is a loosely typed language. 
+
+    Prior to ES6 JS, there was one specific keyword for declaring a variable.
+    1. var
+        After ES6, two new keywords were introduced
+    2. let
+    3. const
+
+    var vs let
+    Variables declared with var have fewer possible scopes than those declared
+    with let. Scope is way of describing where within code execution a variable
+    is considered valid and referenceable. Any variable declared with var can have
+    one of two scopes:
+    1) Function Scope - The variable is scoped to the function it is declared within.
+    2) Global Scope - The variable is scoped to the global session. 
+
+    Variables declared with let and const can be in block scope in addition
+    to the scopes available to var.
+    3) Block Scope - The variable is scoped to a code block (if statement, loop, etc)
+
+    Hoisting - Hoisting is a behavior of variables declared using the 'var' keyword.
+    This behavior causes the declaration (but not the assignment) of a variable
+    to be lifted to the top of its scope.
+*/
+
+var x = 20;
+var name = 'Abby';
+
+let y = 10;
+let lastName = 'Adams';
+lastName = 30;
+
+/*
+    A function is code abstracted to a reusable function that can be called
+    when needed.
+*/
+const myFunction = function() {
+    // Arbitrary block
+    {
+        var myVar = 'hello'; // var will be scoped to the function
+        let myLet = 'world'; // let will be scoped to this block
+        console.log(myLet);
+    }
+    
+    console.log(myVar);
+    // console.log(myLet); // Error - myLet is not defined
+}
+
+// myFunction();
+// console.log(myVar);
+
+const hoistingExample = function() {
+    // var myVar;
+    console.log(myVar);
+    console.log('function starting');
+
+    var myVar = 'hello';
+    // myVar = 'hello';
+    let myLet = 'world';
+    console.log(myVar);
+}
+
+hoistingExample();
